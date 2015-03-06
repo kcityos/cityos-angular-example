d91d6010c852465a7cf3b3abca98fffd0fbaf27d
Damir Zekic <damirz@gmail.com>
2015-03-06 21:38:03 +0100

    Limit the number of displayed points

--- javascripts/main.js
+++ javascripts/main.js
@@ -37,7 +37,7 @@ dataVis.controller('ListController', function($scope, IncomingData) {
   $scope.$watchCollection('data', function(newData) {
     if (chart) {
       // Highcharts has a nasty habit of mutating arrays given to it, so we'll give it a copy
-      newData = newData.slice();
+      newData = newData.slice(Math.max(0, newData.length - 15));
       chart.series[0].setData(newData);
     }
   });
